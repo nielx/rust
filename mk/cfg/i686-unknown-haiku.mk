e4511258789ee6cf16b23105cf6d691ef1c4a9ca
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2017-09-09 15:17:30 +0000

    Haiku x86 build fix: tell the compiler explicitly the arch is i686
    Otherwise cmake in compiler-rt will fail on this platform. This change
    does not affect x86_64.

--- mk/cfg/i686-unknown-haiku.mk
+++ mk/cfg/i686-unknown-haiku.mk
@@ -9,7 +9,7 @@ CFG_STATIC_LIB_NAME_i686-unknown-haiku=lib$(1).a
 CFG_LIB_GLOB_i686-unknown-haiku=lib$(1)-*.so
 CFG_LIB_DSYM_GLOB_i686-unknown-haiku=lib$(1)-*.dylib.dSYM
 CFG_CFLAGS_i686-unknown-haiku := -m32 $(CFLAGS)
-CFG_GCCISH_CFLAGS_i686-unknown-haiku := -Wall -Werror -g -fPIC -m32 $(CFLAGS)
+CFG_GCCISH_CFLAGS_i686-unknown-haiku := -Wall -Werror -g -fPIC -m32 $(CFLAGS) -march=i686
 CFG_GCCISH_CXXFLAGS_i686-unknown-haiku := -fno-rtti $(CXXFLAGS)
 CFG_GCCISH_LINK_FLAGS_i686-unknown-haiku := -shared -fPIC -ldl -pthread  -lrt -g -m32
 CFG_GCCISH_PRE_LIB_FLAGS_i686-unknown-haiku := -Wl,-whole-archive
