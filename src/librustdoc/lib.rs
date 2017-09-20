0c303e808e373675537a76d2570844af92c0cba9
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2017-09-20 17:36:59 +0000

    Haiku: reduce stack memory to 16MB (max on Haiku)

--- src/librustdoc/lib.rs
+++ src/librustdoc/lib.rs
@@ -101,7 +101,7 @@ struct Output {
 }
 
 pub fn main() {
-    const STACK_SIZE: usize = 32_000_000; // 32MB
+    const STACK_SIZE: usize = 16_000_000; // 16MB on Haiku
     env_logger::init();
     let res = std::thread::Builder::new().stack_size(STACK_SIZE).spawn(move || {
         syntax::with_globals(move || {
