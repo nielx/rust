b119cb6a3e00bacc275f6ddf29075fdb0454597d
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2018-05-09 17:31:41 +0000

    Haiku: there is no setpriority on this platform.

--- src/bootstrap/lib.rs
+++ src/bootstrap/lib.rs
@@ -168,7 +168,7 @@ mod tool;
 #[cfg(windows)]
 mod job;
 
-#[cfg(unix)]
+#[cfg(all(unix, not(target_os = "haiku")))]
 mod job {
     use libc;
 
@@ -179,7 +179,7 @@ mod job {
     }
 }
 
-#[cfg(not(any(unix, windows)))]
+#[cfg(any(target_os = "haiku", not(any(unix, windows))))]
 mod job {
     pub unsafe fn setup(_build: &mut ::Build) {
     }
